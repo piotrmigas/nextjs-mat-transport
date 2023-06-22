b6f9abd318640e4970fa8d42be9271d2249af9a4
piotrmigas <piotr.migas@icloud.com>
2023-06-22 15:13:37 +0200

    tel validation

--- components/Form.tsx
+++ components/Form.tsx
@@ -104,6 +104,10 @@ const Form = () => {
                 type='tel'
                 {...register('phone', {
                   required: true,
+                  pattern: {
+                    value: /^(?:0\.(?:0[0-9]|[0-9]\d?)|[0-9]\d*(?:\.\d{1,2})?)(?:e[+-]?\d+)?$/,
+                    message: '',
+                  },
                 })}
                 id='phone'
                 className={`mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md ${
